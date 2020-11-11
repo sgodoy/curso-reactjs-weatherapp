f735898a83f8cf5f3bc9c676c3a1208874979378
Sergio Godoy <sergio.igg@gmail.com>
2020-11-11 17:26:45 -0300

    Inicio sección Rutas

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>  
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        Welcome
+                    </Route>
+                    <Route path='/main'>
+                        Main
+                    </Route>
+                    <Route path='/city'>
+                        City
+                    </Route>
+                    <Route>
+                        Not Found
+                    </Route>
+                </Switch>
+            </Router> 
+        </div>
+    )
+}
+
+export default App
